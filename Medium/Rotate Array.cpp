b64bca582d36cc15f68c9e5b2e9b8bc483f5c463
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-06-23 16:47:35 +0530

    Create Rotate Array.cpp
    first, divide the array into two parts by 0,k-1, and k, the size of the array  
    Rotate each part of the array and then rotate the whole array.

---
+++ Medium/Rotate Array.cpp
@@ -0,0 +1,19 @@
+//189. Rotate Array
+//https://leetcode.com/problems/rotate-array/description/
+class Solution {
+public:
+    void reverse(vector<int>& nums,int low,int high){
+        while(low<high){
+            swap(nums[low],nums[high]);
+            low++;
+            high--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums,0,n-1-k);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+};
