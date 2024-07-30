67cee48a5b6ff0c690045e2965b79bdf2b37df23
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-07-31 02:11:46 +0530

    Create Sort Colors.java

---
+++ Medium/Sort Colors.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/sort-colors/description/
+import java.util.*;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int mid=0;
+        int high =n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
