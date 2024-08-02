9e645ea7219759df7197044f13035f4abdd38f3a
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-02 23:36:32 +0530

    Create Maximum Subarray.java

---
+++ Medium/Maximum Subarray.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/maximum-subarray/description/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int max =Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>max) max= sum;
+            if(sum<0) sum=0;
+        }
+        return max;
+    }
+}
