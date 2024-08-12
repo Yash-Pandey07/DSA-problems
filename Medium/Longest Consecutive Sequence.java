d412a28050790b0e3b56884af0575ad0f9513b9c
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-12 12:22:47 +0530

    Create Longest Consecutive Sequence.java

---
+++ Medium/Longest Consecutive Sequence.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/description/
+class Solution {
+    public int longestConsecutive(int[] a) {
+    int n = a.length;
+        if (n == 0) return 0;
+
+        //sort the array:
+        Arrays.sort(a);
+        int lastSmaller = Integer.MIN_VALUE;
+        int cnt = 0;
+        int longest = 1;
+
+        //find longest sequence:
+        for (int i = 0; i < n; i++) {
+            if (a[i] - 1 == lastSmaller) {
+                //a[i] is the next element of the
+                //current sequence.
+                cnt += 1;
+                lastSmaller = a[i];
+            } else if (a[i] != lastSmaller) {
+                cnt = 1;
+                lastSmaller = a[i];
+            }
+            longest = Math.max(longest, cnt);
+        }
+        return longest;
+    }
+}
