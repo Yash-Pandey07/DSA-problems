77647713a9d79642cf4187c4e7fb8e49ba895514
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-12 12:37:35 +0530

    Create Longest Consecutive Sequence(Optimal).java

---
+++ Medium/Longest Consecutive Sequence(Optimal).java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/description/
+class Solution {
+    public int longestConsecutive(int[] a) {
+        int n = a.length;
+        if (n == 0)
+            return 0;
+
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+
+        // put all the array elements into set
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
+        }
+
+        // Find the longest sequence
+        for (int it : set) {
+            // if 'it' is a starting number
+            if (!set.contains(it - 1)) {
+                // find consecutive numbers
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+}
