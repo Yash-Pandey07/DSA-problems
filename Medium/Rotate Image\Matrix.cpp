77b6129d7bea318bce5bdf395b347ba69f74ed1f
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-16 01:09:15 +0530

    Create Rotate Image\Matrix.cpp

---
+++ Medium/Rotate Image\Matrix.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/rotate-image/description/
+//https://www.naukri.com/code360/problems/rotate-the-matrix_6825090
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+    int n= matrix.size();
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			swap(matrix[i][j], matrix[j][i]);
+		}
+	}
+
+	for(int i=0;i<n;i++){
+		reverse(matrix[i].begin(),matrix[i].end());
+	}
+    }
+};
