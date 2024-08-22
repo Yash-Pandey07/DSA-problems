f5f5d20a339179834c2104c4451dad50bdc83e75
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-22 23:13:35 +0530

    Create Pascal's Triangle.cpp

---
+++ Hard/Pascal's Triangle.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/pascals-triangle/
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
