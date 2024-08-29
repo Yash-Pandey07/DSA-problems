29f4e8c28ff9829a3917b094fbc4956ccb6e0d6e
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-08-29 22:58:46 +0530

    Create  Longest Subarray With Zero Sum.cpp

---
+++ Hard/Longest Subarray With Zero Sum.cpp
@@ -0,0 +1,35 @@
+//https://www.naukri.com/code360/problems/longest-subarray-with-zero-sum_6783450
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& a) {
+    int maxLen = 0;
+    unordered_map<int, int> sumIndexMap;
+    int sum = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i];
+
+        if (sum == 0) {
+            maxLen = i + 1;
+        } else if (sumIndexMap.find(sum) != sumIndexMap.end()) {
+            maxLen = max(maxLen, i - sumIndexMap[sum]);
+        } else {
+            sumIndexMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    vector<int> a = {9, -3, 3, -1, 6, -5};
+    cout << solve(a) << endl;
+
+    return 0;
+}
+
+
