7423181911102603abef46cfd5ad89964128ec42
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-09-05 23:32:56 +0530

    Create Maximum Product Subarray.java

---
+++ Medium/Maximum Product Subarray.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/maximum-product-subarray/description/
+
+
+
+import java.util.*;
+public class Main
+{
+	static int maxProductSubArray(int arr[]) {
+	    int result = Integer.MIN_VALUE;
+	    for(int i=0;i<arr.length-1;i++) 
+	        for(int j=i+1;j<arr.length;j++) {
+	            int prod = 1;
+	            for(int k=i;k<=j;k++) 
+	                prod *= arr[k];
+	            result = Math.max(result,prod);
+	        }
+	   return result;     
+	}
+	public static void main(String[] args) {
+		int nums[] = {1,2,-3,0,-4,-5};
+		int answer = maxProductSubArray(nums);
+		System.out.print("The maximum product subarray is: "+answer);
+	}
+}
+
