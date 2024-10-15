af6db64ebaa281e3ba0ac1214cf26a79e7461635
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-10-15 16:43:13 +0530

    Create Find Nth root of M.java

---
+++ Binary search /Find Nth root of M.java
@@ -0,0 +1,32 @@
+//https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+
+class Solution
+{
+    public static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+    
+    public int NthRoot(int n, int m)
+    {
+        // code here
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
