f4f6c61a6e368d1ada6c69b2f525aabe467fbe96
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-10-23 23:47:01 +0530

    Create Capacity To Ship Packages Within D Days.java

---
+++ Binary search /Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+class Solution {
+     public static int findDays(int[] weights, int cap) {
+        int days = 1; //First day.
+        int load = 0;
+        int n = weights.length; //size of array.
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1; //move to next day
+                load = weights[i]; //load the weight.
+            } else {
+                //load the weight on the same day.
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+          //Find the maximum and the summation:
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int i = 0; i < weights.length; i++) {
+            high += weights[i];
+            low = Math.max(low, weights[i]);
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+            if (numberOfDays <= days) {
+                //eliminate right half
+                high = mid - 1;
+            } else {
+                //eliminate left half
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
