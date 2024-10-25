87c999cd4f55c06808759b9a97203d424cb1b5bb
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-10-25 15:01:59 +0530

    Update Kth Missing Positive Number.cpp

--- Binary search /Kth Missing Positive Number.cpp
+++ Binary search /Kth Missing Positive Number.cpp
@@ -2,9 +2,23 @@
 //Brute Force Approach
 int missingK(vector < int > vec, int n, int k) {
     // Write your code here.
-    for(int i=0;i<n;i++){
-        if(vec[i] <= k)k++;
-        else break;
+    //brute force
+    // for(int i=0;i<n;i++){
+    //     if(vec[i] <= k)k++;
+    //     else break;
+    // }
+    // return k;
+
+    //BS
+    int low=0, high=n-1;
+    while(low<= high){
+        int mid = (low+high)/2;
+        int missing = vec[mid]- (mid+1);
+        if(missing <k){
+            low= mid+1;
+        }
+        else high= mid-1;
     }
-    return k;
+   // return high +1 + k;           //for edge case we should not use this as high can be negative number too.
+    return low +k;
 }
