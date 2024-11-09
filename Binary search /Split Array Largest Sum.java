7f4f3a4391c7d82a0ae84d18c2644a9d6e660155
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-11-09 17:11:25 +0530

    Create Split Array Largest Sum.java

---
+++ Binary search /Split Array Largest Sum.java
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/split-array-largest-sum/
+class Solution {
+public static int countPartitions(int[] a, int maxSum) {
+        int n = a.length; //size of array.
+        int partitions = 1;
+        long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                //insert element to current subarray
+                subarraySum += a[i];
+            } else {
+                //insert element to next subarray
+                partitions++;
+                subarraySum = a[i];
+            }
+        }
+        return partitions;
+    }
+
+    public static int largestSubarraySumMinimized(int[] a, int k) {
+        int low = a[0];
+        int high = 0;
+        //find maximum and summation:
+        for (int i = 0; i < a.length; i++) {
+            low = Math.max(low, a[i]);
+            high += a[i];
+        }
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(a, mid);
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        return largestSubarraySumMinimized(nums ,k);
+    }
+}
