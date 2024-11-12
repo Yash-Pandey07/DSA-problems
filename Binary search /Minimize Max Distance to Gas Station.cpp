cb8d0020c80a5e0f6f48206a36cf37511f7fe1b2
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-11-13 01:32:42 +0530

    Update Minimize Max Distance to Gas Station.cpp

--- Binary search /Minimize Max Distance to Gas Station.cpp
+++ Binary search /Minimize Max Distance to Gas Station.cpp
@@ -4,37 +4,76 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-//optimal 
+//optimal
+int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size(); // size of the array
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
+        }
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
 
 long double minimiseMaxDistance(vector<int> &arr, int k) {
-    int n = arr.size(); //size of array.
-    vector<int> howMany(n - 1, 0);
-    priority_queue<pair<long double, int>> pq;
+    int n = arr.size(); // size of the array
+    long double low = 0;
+    long double high = 0;
 
-    //insert the first n-1 elements into pq
-    //with respective distance values:
+    //Find the maximum distance:
     for (int i = 0; i < n - 1; i++) {
-        pq.push({arr[i + 1] - arr[i], i});
+        high = max(high, (long double)(arr[i + 1] - arr[i]));
     }
 
-    //Pick and place k gas stations:
-    for (int gasStations = 1; gasStations <= k; gasStations++) {
-        //Find the maximum section
-        //and insert the gas station:
-        auto tp = pq.top();
-        pq.pop();
-        int secInd = tp.second;
-
-        //insert the current gas station:
-        howMany[secInd]++;
-
-        long double inidiff = arr[secInd + 1] - arr[secInd];
-        long double newSecLen = inidiff / (long double)(howMany[secInd] + 1);
-        pq.push({newSecLen, secInd});
+    //Apply Binary search:
+    long double diff = 1e-6 ;
+    while (high - low > diff) {
+        long double mid = (low + high) / (2.0);
+        int cnt = numberOfGasStationsRequired(mid, arr);
+        if (cnt > k) {
+            low = mid;
+        }
+        else {
+            high = mid;
+        }
     }
-
-    return pq.top().first;
+    return high;
 }
+
+
+//better 
+// long double minimiseMaxDistance(vector<int> &arr, int k) {
+//     int n = arr.size(); //size of array.
+//     vector<int> howMany(n - 1, 0);
+//     priority_queue<pair<long double, int>> pq;
+
+//     //insert the first n-1 elements into pq
+//     //with respective distance values:
+//     for (int i = 0; i < n - 1; i++) {
+//         pq.push({arr[i + 1] - arr[i], i});
+//     }
+
+//     //Pick and place k gas stations:
+//     for (int gasStations = 1; gasStations <= k; gasStations++) {
+//         //Find the maximum section
+//         //and insert the gas station:
+//         auto tp = pq.top();
+//         pq.pop();
+//         int secInd = tp.second;
+
+//         //insert the current gas station:
+//         howMany[secInd]++;
+
+//         long double inidiff = arr[secInd + 1] - arr[secInd];
+//         long double newSecLen = inidiff / (long double)(howMany[secInd] + 1);
+//         pq.push({newSecLen, secInd});
+//     }
+
+//     return pq.top().first;
+// }
 
 
 // Brute FOrce
