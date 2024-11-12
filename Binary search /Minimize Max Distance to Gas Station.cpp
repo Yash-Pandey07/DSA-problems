0eac45ddd50e270e1697009a86f8a4174f544b63
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2024-11-12 23:38:12 +0530

    Update Minimize Max Distance to Gas Station.cpp

--- Binary search /Minimize Max Distance to Gas Station.cpp
+++ Binary search /Minimize Max Distance to Gas Station.cpp
@@ -4,40 +4,73 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+//optimal 
 
 long double minimiseMaxDistance(vector<int> &arr, int k) {
     int n = arr.size(); //size of array.
     vector<int> howMany(n - 1, 0);
+    priority_queue<pair<long double, int>> pq;
+
+    //insert the first n-1 elements into pq
+    //with respective distance values:
+    for (int i = 0; i < n - 1; i++) {
+        pq.push({arr[i + 1] - arr[i], i});
+    }
 
     //Pick and place k gas stations:
     for (int gasStations = 1; gasStations <= k; gasStations++) {
         //Find the maximum section
         //and insert the gas station:
-        long double maxSection = -1;
-        int maxInd = -1;
-        for (int i = 0; i < n - 1; i++) {
-            long double diff = arr[i + 1] - arr[i];
-            long double sectionLength =
-                diff / (long double)(howMany[i] + 1);
-            if (sectionLength > maxSection) {
-                maxSection = sectionLength;
-                maxInd = i;
-            }
-        }
+        auto tp = pq.top();
+        pq.pop();
+        int secInd = tp.second;
+
         //insert the current gas station:
-        howMany[maxInd]++;
+        howMany[secInd]++;
+
+        long double inidiff = arr[secInd + 1] - arr[secInd];
+        long double newSecLen = inidiff / (long double)(howMany[secInd] + 1);
+        pq.push({newSecLen, secInd});
     }
 
-    //Find the maximum distance i.e. the answer:
-    long double maxAns = -1;
-    for (int i = 0; i < n - 1; i++) {
-        long double diff = arr[i + 1] - arr[i];
-        long double sectionLength =
-            diff / (long double)(howMany[i] + 1);
-        maxAns = max(maxAns, sectionLength);
-    }
-    return maxAns;
+    return pq.top().first;
 }
+
+
+// Brute FOrce
+// long double minimiseMaxDistance(vector<int> &arr, int k) {
+//     int n = arr.size(); //size of array.
+//     vector<int> howMany(n - 1, 0);
+
+//     //Pick and place k gas stations:
+//     for (int gasStations = 1; gasStations <= k; gasStations++) {
+//         //Find the maximum section
+//         //and insert the gas station:
+//         long double maxSection = -1;
+//         int maxInd = -1;
+//         for (int i = 0; i < n - 1; i++) {
+//             long double diff = arr[i + 1] - arr[i];
+//             long double sectionLength =
+//                 diff / (long double)(howMany[i] + 1);
+//             if (sectionLength > maxSection) {
+//                 maxSection = sectionLength;
+//                 maxInd = i;
+//             }
+//         }
+//         //insert the current gas station:
+//         howMany[maxInd]++;
+//     }
+
+//     //Find the maximum distance i.e. the answer:
+//     long double maxAns = -1;
+//     for (int i = 0; i < n - 1; i++) {
+//         long double diff = arr[i + 1] - arr[i];
+//         long double sectionLength =
+//             diff / (long double)(howMany[i] + 1);
+//         maxAns = max(maxAns, sectionLength);
+//     }
+//     return maxAns;
+// }
 
 int main()
 {
