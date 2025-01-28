05d48b1734ae63cff98036aed0475401b2bdaba5
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2025-01-28 23:50:11 +0530

    Create 271 - Encode and Decode Strings.java

---
+++ Medium/271 - Encode and Decode Strings.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/encode-and-decode-strings/description/
+//https://leetcode.ca/2016-08-27-271-Encode-and-Decode-Strings/
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder ans = new StringBuilder();
+        for (String s : strs) {
+            ans.append((char) s.length()).append(s);
+        }
+        return ans.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ans = new ArrayList<>();
+        int i = 0, n = s.length();
+        while (i < n) {
+            int size = s.charAt(i++);
+            ans.add(s.substring(i, i + size));
+            i += size;
+        }
+        return ans;
+    }
+}
