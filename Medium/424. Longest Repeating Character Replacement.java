ff4796a4a084ced1e05aa3d0fb868c95a67150e5
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2025-02-08 00:17:38 +0530

    Update 424. Longest Repeating Character Replacement.java

--- Medium/424. Longest Repeating Character Replacement.java
+++ Medium/424. Longest Repeating Character Replacement.java
@@ -1,2 +1,20 @@
 //https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
+class Solution {
+    public int characterReplacement(String s, int k) {
+          
+          int[] occurance = new int[26];
+          int left = 0 ,right =0 ,ans = 0 , maxOccurance =0;
+
+          for( right=0 ; right< s.length();right++){
+            maxOccurance = Math.max(maxOccurance, ++occurance[s.charAt(right) - 'A']);
+            if(right - left +1 -maxOccurance > k){
+                
+                occurance[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, right-left +1);
+          }
+          return ans;
+    }
+}
